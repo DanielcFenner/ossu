445425ad3e7f6641781bee0259e67653cf78c59b
DanielFenner <dcfenner@gmail.com>
2022-06-12 12:34:01 -0400

    unit 2 nearly complete

--- introduction-to-computer-science-mit-6-00-1/unit-2-simple-programs/gcd_recur.py
+++ introduction-to-computer-science-mit-6-00-1/unit-2-simple-programs/gcd_recur.py
@@ -4,5 +4,9 @@ def gcdRecur(a, b):
     
     returns: a positive integer, the greatest common divisor of a & b.
     '''
-    # Your code here
-    
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a%b)
+
+print(gcdRecur(2, 12))
